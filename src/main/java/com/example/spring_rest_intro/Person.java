ec6efcb173665f1dfcd303561b510bf427e6f2c5
nz-bill <bill.palmestedt@zocom.se>
2025-05-09 13:16:49 +0200

    adding relations

--- src/main/java/com/example/spring_rest_intro/Person.java
+++ src/main/java/com/example/spring_rest_intro/Person.java
@@ -1,10 +1,7 @@
 package com.example.spring_rest_intro;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Person {
---
+++ src/main/java/com/example/spring_rest_intro/Profil.java
@@ -0,0 +1,4 @@
+package com.example.spring_rest_intro;
+
+public class Profil {
+}
